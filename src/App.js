5726054761a724db14df08b01432cbccdd9cf880
Maks-Chornyi <maksym.chornyi@oa.edu.ua>
2019-04-21 19:32:52 +0300

    implemented getting data by api

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React from 'react';
-import Info from './components/info'
+import Info from './components/info';
+import Form from './components/form';
+import Weater from './components/weather';
+
+const API_KEY = "38437bbf7af1021d0e45229180c767c6";
 
 class App extends React.Component {
+
+getWeather = async () => {
+  const api_url = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=Lviv,ua&appid=${API_KEY}&units=metric`);
+  const data = await api_url.json();
+  console.log(data);
+}
+
   render() {
     return (
       <div>
         <Info />
+        <Form />
+        <Weather />
       </div>
     );
   }
---
+++ src/components/weather.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Weather extends React.Component {
+    render() {
+        return (
+            <dvi>Weater info's component</dvi>
+        );
+    }
+}
+
+export default Weather;
