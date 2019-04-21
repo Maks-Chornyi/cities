4c3cbd213a4b0fcaf4269de25ca79e74e7050b7f
Maks-Chornyi <maksym.chornyi@oa.edu.ua>
2019-04-21 16:33:29 +0300

    created new component, added this comp to app js for rendering it on the page

---
+++ src/components/info.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Info extends React.Component {
+    render () {
+        return (
+            <div>
+            <h2>Weather App</h2>
+            <p>Let you know weahter in Lviv</p>
+            </div>
+        );
+    }
+}
+
+export default Info;
